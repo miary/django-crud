25e3883288102f6c87004298500326aca5d8d6d1
Miary Andria <themia@gmail.com>
2023-12-12 21:53:44 -0800

    tables

--- yaml/urls.py
+++ yaml/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from iommi import Form, Table
-
+from .views import IndexPage
 from .models import Bot, Intent, Response, Story
 
 urlpatterns = [
+    path('', IndexPage().as_view()),
     path('io-bot/', Form.create(auto__model=Bot).as_view()),
     path('io-form/', Form.create(auto__model=Intent).as_view()),
+    path('io-resp/', Form.create(auto__model=Response).as_view()),
     path('io-table/',Table(auto__model=Intent).as_view()),
 ]
--- yaml/views.py
+++ yaml/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from iommi import Page, html, Table, Action, Column
+from .models import Intent, Bot, Response
 
-# Create your views here.
+class IndexPage(Page):
+    title = html.h1('Data Management')
+    welcome_text = 'Create intent, response, and stories'
+
+    Intent = Table(
+        auto__model=Intent, 
+        actions__create_intent=Action(
+    attrs__href='/intents/create/',
+    include=lambda request, **_: request.user.is_staff,
+),
+columns__edit=Column.edit(
+    after=0,
+    include=lambda request, **_: request.user.is_staff,
+),
+columns__delete=Column.delete(
+    include=lambda request, **_: request.user.is_staff,
+),
+)
+    
