02b969e5d54cb81f67748af440b721bfb1d4ffc3
Miary Andria <themia@gmail.com>
2024-01-03 07:03:43 -0800

    git

--- yaml/urls.py
+++ yaml/urls.py
@@ -6,7 +6,7 @@ from .models import Bot, Intent, Response, Story
 urlpatterns = [
     path('', IndexPage().as_view()),
     path('io-bot/', Form.create(auto__model=Bot).as_view()),
-    path('io-form/', Form.create(auto__model=Intent).as_view()),
+    path('io-intent/', Form.create(auto__model=Intent).as_view()),
     path('io-resp/', Form.create(auto__model=Response).as_view()),
     path('io-table/',Table(auto__model=Intent).as_view()),
 ]
--- yaml/views.py
+++ yaml/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from iommi import Page, html, Table, Action, Column
 from .models import Intent, Bot, Response
 
+
 class IndexPage(Page):
-    title = html.h1('Data Management')
+    title = html.h1("Chatbot Management")
     welcome_text = 'Create intent, response, and stories'
 
     Intent = Table(
         auto__model=Intent, 
+        columns__name__filter__include=True,
         actions__create_intent=Action(
-    attrs__href='/intents/create/',
-    include=lambda request, **_: request.user.is_staff,
+            attrs__href='/io-intent/'
 ),
 columns__edit=Column.edit(
     after=0,
